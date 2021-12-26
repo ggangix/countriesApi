be1c2069642577d1e2f011e2698ee8b436824ccd
Giuseppe <ggangix@gmail.com>
2021-12-26 12:12:18 +0100

    Add routes to server file

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require('express');
 require('dotenv').config();
 require('./config/db.js');
 const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+const countryRoutes = require('./routes/countryRoutes.js');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.route('/').get((req, res) => {
-  console.log('works');
+  res.json({ message: 'Please use /countries' });
 });
 
+countryRoutes(app);
+userRoutes(app);
 
 const port = process.env.PORT;
 
